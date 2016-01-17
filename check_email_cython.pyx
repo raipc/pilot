af6f499ba3c2eb76cbc24e22e1df6ac0d1a21d02
Anton Rybochkin <anton-rib@mail.ru>
2016-01-17 06:32:41 +0300

    twice faster performance of Cython implementation

--- check_email_cython.pyx
+++ check_email_cython.pyx
@@ -6,21 +6,30 @@ cdef int MIN_DOMAIN_LENGTH = 3
 cdef int MAX_DOMAIN_LENGTH = 256
 
 
-cdef bint allowed_char(Py_UCS4 ch, bint inside_quotes):
-    return u'a' <= ch <= u'z' or u'0'<= ch <= u'9' or ch in u'_-' or (
-        inside_quotes and ch in u'!,:')
+cdef bint allowed_char(char ch, bint inside_quotes):
+    return 'a' <= ch <= 'z' or '0'<= ch <= '9' or ch == '_' or ch == '-' or (
+        inside_quotes and (ch == '!' or ch == ',' or ch == ':'))
 
 
-def check_email(basestring input):
-    if len(input) > MAX_NAME_LENGTH + MAX_DOMAIN_LENGTH + 1:
+def check_email(basestring inp):
+    if len(inp) > MAX_NAME_LENGTH + MAX_DOMAIN_LENGTH + 1:
         return False
     cdef bint name_part, open_quote
     cdef int char_counter = 0
-    cdef Py_UCS4 prev, ch
-    cdef unicode email = unicode(input)
+    cdef char prev, ch
+    cdef char* email
+    cdef bytes encoded_str
+    if isinstance(inp, unicode):
+        try:
+            encoded_str = inp.encode()
+            email = encoded_str
+        except UnicodeEncodeError:
+            return False
+    else:
+        email = inp
     name_part = True
     open_quote = False
-    prev = u'0'
+    prev = '0'
     for ch in email:
         char_counter += 1
 
@@ -28,27 +37,26 @@ def check_email(basestring input):
             if allowed_char(ch, open_quote):
                 prev = ch
                 continue
-            if ch == u'.':
-                if prev == u'.':
+            if ch == '.':
+                if prev == '.':
                     return False
-            elif ch == u'@':
+            elif ch == '@':
                 if char_counter == 1 or char_counter > 129 or open_quote:
                     return False
                 name_part = False
                 char_counter = 0
-            elif ch == u"\"":
+            elif ch == '"':
                 open_quote = not open_quote
             else:
                 return False
         else:
-            if ch == u'.':
-                if prev == u'.' or prev == u'-' or prev == u'@':
+            if ch == '.':
+                if prev == '.' or prev == '-' or prev == '@':
                     return False
-            elif ch == u'-':
-                if prev == u'.' or prev == u'@':
+            elif ch == '-':
+                if prev == '.' or prev == '@':
                     return False
             elif not allowed_char(ch, False):
                 return False
         prev = ch
-    return name_part == 0 and 3 <= char_counter <= 256 and ch not in u'.-'
-
+    return not name_part and 3 <= char_counter <= 256 and ch != '.' and ch != '-'
